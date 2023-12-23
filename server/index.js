f1bdecaff6fe48ce87be4d8f878312427ef24d11
NirajSrivastava18 <nirajsrivastava2001@gmail.com>
2023-12-23 22:46:29 +0530

    deployment changes

--- server/index.js
+++ server/index.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+dotenv.config();
 const mongoose = require('mongoose');
 const userRoute = require('./routes/userRoute');
 const jobRoute = require('./routes/jobRoute');
 const cors = require('cors');
-
-dotenv.config();
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 app.use('/', userRoute);
 app.use('/', jobRoute);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
   let time = new Date().toLocaleTimeString();
   res.json({ time: time, app: 'Job-Listing-server', state: 'active' });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 
 app.listen(process.env.PORT, (res, req) => {
   mongoose
