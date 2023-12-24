0701c73e2778a6c935dc9cf2a1b2647dec0d1a40
NirajSrivastava18 <nirajsrivastava2001@gmail.com>
2023-12-24 18:49:14 +0530

    Changes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors((origin = 'https://job-listing-rouge.vercel.app/jobs')));
+app.use(cors((origin = 'https://job-listing-rouge.vercel.app')));
 app.use('/', userRoute);
 app.use('/', jobRoute);
 
