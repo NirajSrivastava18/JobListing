f96cdd1fa86b527561d5a4b4bc0c5846da096afe
NirajSrivastava18 <nirajsrivastava2001@gmail.com>
2023-12-24 17:52:19 +0530

    Changes

--- server/index.js
+++ server/index.js
@@ -20,10 +20,8 @@ app.get('/', (req, res) => {
   let time = new Date().toLocaleTimeString();
   res.json({ time: time, app: 'Job-Listing-server', state: 'active' });
 });
-
-const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
